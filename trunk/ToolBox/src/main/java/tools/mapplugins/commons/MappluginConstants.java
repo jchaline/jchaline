37b43b44a1118b145abb3bd236d38384508e8c8f
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-03-14 22:38:43 +0000

    Begin SQL generating implementation

--- trunk/ToolBox/src/main/java/tools/mapplugins/commons/MappluginConstants.java
+++ trunk/ToolBox/src/main/java/tools/mapplugins/commons/MappluginConstants.java
@@ -3,4 +3,5 @@ package tools.mapplugins.commons;
 public class MappluginConstants
 {
     public static final String MARK_SVN_URL ="mapplugin.svn.url";
+    public static final String MARK_SQL_FILE ="mapplugin.sql.file";
 }
