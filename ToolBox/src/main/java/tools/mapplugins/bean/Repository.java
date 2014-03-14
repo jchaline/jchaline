c979b8e6568dd5f469a4345fddf185074aaf6d69
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-03-14 22:30:31 +0000

    Add pom parsing with jsoup

--- ToolBox/src/main/java/tools/mapplugins/bean/Repository.java
+++ ToolBox/src/main/java/tools/mapplugins/bean/Repository.java
@@ -1,6 +1,8 @@
 package tools.mapplugins.bean;
 
+import java.util.HashMap;
 import java.util.Map;
+
 
 /**
  * Handling artifacts
@@ -11,12 +13,30 @@ public class Repository
     /**
      * Reference all the artifact, getting by group id, id, and version
      */
-    private Map<String, Map<String, Map<String,Artifact>>> artifacts;
+    private Map<String, Map<String, Map<String, ArtifactVersion>>> artifacts;
+
+    /**
+     * The way to add artifact to repository
+     * @param artifact the artifact to add
+     */
+    public void add( ArtifactVersion artifact )
+    {
+        if ( getArtifacts( ).containsKey( artifact.getGroupId( ) ) )
+        {
+            getArtifacts( ).put( artifact.getGroupId( ), new HashMap<String, Map<String, ArtifactVersion>>( ) );
+        }
+        if ( getArtifacts( ).get( artifact.getGroupId( ) ).containsKey( artifact.getId( ) ) )
+        {
+            getArtifacts( ).get( artifact.getGroupId( ) ).put( artifact.getId( ),
+                    new HashMap<String, ArtifactVersion>( ) );
+        }
+        getArtifacts( ).get( artifact.getGroupId( ) ).get( artifact.getId( ) ).put( artifact.getVersion( ), artifact );
+    }
 
     /**
      * @return the artifacts
      */
-    public Map<String, Map<String, Map<String,Artifact>>> getArtifacts( )
+    public Map<String, Map<String, Map<String, ArtifactVersion>>> getArtifacts( )
     {
         return artifacts;
     }
@@ -24,7 +44,7 @@ public class Repository
     /**
      * @param artifacts the artifacts to set
      */
-    public void setArtifacts( Map<String, Map<String, Map<String,Artifact>>> artifacts )
+    public void setArtifacts( Map<String, Map<String, Map<String, ArtifactVersion>>> artifacts )
     {
         this.artifacts = artifacts;
     }
