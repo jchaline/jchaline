a316b033787debd5deb0232d8194db2beef8661c
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-03-19 11:42:48 +0000

    Initial import : src JUnit

---
+++ plugin-pac/src/test/java/fr/paris/lutece/plugins/pac/utils/commons/DateUtilsTest.java
@@ -0,0 +1,29 @@
+package fr.paris.lutece.plugins.pac.utils.commons;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import fr.paris.lutece.test.LuteceTestCase;
+
+/**
+ * JUnit for @link{DateUtils} 
+ * @author jchaline
+ */
+public class DateUtilsTest extends LuteceTestCase
+{
+    public void testDateCompare( )
+    {
+        GregorianCalendar cal = new GregorianCalendar( );
+        cal.set( 2013, 12, 31 );
+        Date dayOne = cal.getTime( );
+
+        cal.set( 2013, 12, 30 );
+        Date dayTwo = cal.getTime( );
+
+        cal.set( 2014, 1, 1 );
+        Date dayThree = cal.getTime( );
+        assertEquals( DateUtils.compare( dayOne, dayTwo ), 1 );
+        assertEquals( DateUtils.compare( dayThree, dayTwo ), 1 );
+        assertEquals( DateUtils.compare( dayOne, dayThree ), -1 );
+    }
+}
