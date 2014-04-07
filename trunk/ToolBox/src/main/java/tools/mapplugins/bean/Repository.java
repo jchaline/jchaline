563ddff476f799ba42d7de0aad5f5fccd043f65c
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-04-07 06:09:50 +0000

    Improve mapplugin tool

--- trunk/ToolBox/src/main/java/tools/mapplugins/bean/Repository.java
+++ trunk/ToolBox/src/main/java/tools/mapplugins/bean/Repository.java
@@ -1,42 +1,69 @@
 package tools.mapplugins.bean;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import tools.mapplugins.xml.Project;
 
 
 /**
  * Handling artifacts
  * @author jchaline
  */
-public class Repository
+public class Repository implements Serializable
 {
+    private static final long serialVersionUID = 8115463804253023495L;
     /**
      * Reference all the artifact, getting by group id, id, and version
      */
-    private Map<String, Map<String, Map<String, ArtifactVersion>>> artifacts;
+    private Map<String, Map<String, Map<String, Project>>> artifacts = new HashMap<String, Map<String, Map<String, Project>>>( );
+
+    /**
+     * Get all the artifacts
+     * @return the artifacts list
+     */
+    public List<Project> getProjectsList( )
+    {
+        List<Project> list = new ArrayList<Project>( );
+        for ( Map<String, Map<String, Project>> groups : artifacts.values( ) )
+        {
+            for ( Map<String, Project> versions : groups.values( ) )
+            {
+                list.addAll( versions.values( ) );
+            }
+        }
+
+        return list;
+    }
 
     /**
      * The way to add artifact to repository
      * @param artifact the artifact to add
      */
-    public void add( ArtifactVersion artifact )
+    public void add( Project artifact )
     {
-        if ( getArtifacts( ).containsKey( artifact.getGroupId( ) ) )
+        if ( !getArtifacts( ).containsKey( artifact.getGroupId( ) ) )
         {
-            getArtifacts( ).put( artifact.getGroupId( ), new HashMap<String, Map<String, ArtifactVersion>>( ) );
+            getArtifacts( ).put( artifact.getGroupId( ), new HashMap<String, Map<String, Project>>( ) );
         }
-        if ( getArtifacts( ).get( artifact.getGroupId( ) ).containsKey( artifact.getId( ) ) )
+        if ( !getArtifacts( ).get( artifact.getGroupId( ) ).containsKey( artifact.getArtifactId( ) ) )
         {
-            getArtifacts( ).get( artifact.getGroupId( ) ).put( artifact.getId( ),
-                    new HashMap<String, ArtifactVersion>( ) );
+            getArtifacts( ).get( artifact.getGroupId( ) ).put( artifact.getArtifactId( ),
+                    new HashMap<String, Project>( ) );
         }
-        getArtifacts( ).get( artifact.getGroupId( ) ).get( artifact.getId( ) ).put( artifact.getVersion( ), artifact );
+        getArtifacts( ).get( artifact.getGroupId( ) ).get( artifact.getArtifactId( ) )
+                .put( artifact.getVersion( ), artifact );
     }
 
     /**
      * @return the artifacts
      */
-    public Map<String, Map<String, Map<String, ArtifactVersion>>> getArtifacts( )
+    public Map<String, Map<String, Map<String, Project>>> getArtifacts( )
     {
         return artifacts;
     }
@@ -44,8 +71,27 @@ public class Repository
     /**
      * @param artifacts the artifacts to set
      */
-    public void setArtifacts( Map<String, Map<String, Map<String, ArtifactVersion>>> artifacts )
+    public void setArtifacts( Map<String, Map<String, Map<String, Project>>> artifacts )
     {
         this.artifacts = artifacts;
+    }
+
+    public Project findDependency( String groupId, String artifactId, String version )
+    {
+        Project dependency = null;
+        if ( this.artifacts.containsKey( groupId ) )
+        {
+            Map<String, Map<String, Project>> group = this.artifacts.get( groupId );
+            if ( group.containsKey( artifactId ) )
+            {
+                Map<String, Project> versions = group.get( artifactId );
+                Set<String> keySet = versions.keySet( );
+                List<String> listVersion = new ArrayList<String>(keySet);
+                Collections.sort( listVersion, new ArtifactComparator( ) );
+                System.out.println(listVersion);
+            }
+        }
+
+        return dependency;
     }
 }
--- trunk/ToolBox/src/main/java/tools/mapplugins/commons/MappluginConstants.java
+++ trunk/ToolBox/src/main/java/tools/mapplugins/commons/MappluginConstants.java
@@ -2,6 +2,14 @@ package tools.mapplugins.commons;
 
 public class MappluginConstants
 {
-    public static final String MARK_SVN_URL ="mapplugin.svn.url";
-    public static final String MARK_SQL_FILE ="mapplugin.sql.file";
+    public static final String MARK_SVN_URLS = "mapplugin.svn.urls";
+    public static final String MARK_SERIALIZABLE_FILE = "mapplugin.serializable.file";
+    public static final String MARK_SQL_FILE = "mapplugin.sql.file";
+    public static final String MARK_SQL_FIRST_ID = "mapplugin.sql.first.id";
+    public static final String MARK_SQL_ARTIFACT_TABLE = "mapplugin.sql.artifact.table";
+    public static final String MARK_XML_ATTRIBUTES_REMOVED = "mapplugin.xml.attributes.removed";
+    public static final String MARK_FOLDER_BLACKLISTED = "mapplugin.folder.blacklisted";
+    
+
+    public static final String PROPERTIES_SEPARATOR = ",";
 }
