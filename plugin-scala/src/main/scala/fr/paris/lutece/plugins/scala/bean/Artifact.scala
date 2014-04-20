37033ba494c3cc2f9dfce9e46fe7b722fb567b01
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-04-20 12:11:32 +0000

    Complete the implementation for Lutece plugin write with Scala (compiler 2.10)

--- plugin-scala/src/main/scala/fr/paris/lutece/plugins/scala/bean/Artifact.scala
+++ plugin-scala/src/main/scala/fr/paris/lutece/plugins/scala/bean/Artifact.scala
@@ -1,14 +1,24 @@
 package fr.paris.lutece.plugins.scala.bean
 
 import org.hibernate.validator.constraints.NotEmpty
+import fr.paris.lutece.plugins.genericjpa.bean.AbstractBean
 
-class Artifact {
-  
+class Artifact extends AbstractBean[Int] {
+
+  var _id = 0;
   @NotEmpty
-  var version="";
-  var group="";
-  var id="";
-  
-  
+  var _version = "";
+  @NotEmpty
+  var _groupId = "";
+  @NotEmpty
+  var _artifactId = "";
+
+  def getId(): Int= {
+    _id
+  }
+
+  def setId(id: Int): Unit = {
+    _id = id
+  }
 
 }
---
+++ plugin-scala/src/main/scala/fr/paris/lutece/plugins/scala/service/ScalaPlugin.scala
@@ -0,0 +1,8 @@
+package fr.paris.lutece.plugins.scala.service
+
+import fr.paris.lutece.portal.service.plugin.PluginDefaultImplementation
+
+@serializable
+object ScalaPlugin extends PluginDefaultImplementation {
+  def PLUGIN_NAME = { "scala" }
+}
