9bf589437aa6dcce3a23678def1d1e6847d9fb43
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-04-21 13:28:56 +0000

    Begin to implement tests units, fail for moment

--- trunk/plugin-scala/src/main/scala/fr/paris/lutece/plugins/scala/bean/graph/Graph.scala
+++ trunk/plugin-scala/src/main/scala/fr/paris/lutece/plugins/scala/bean/graph/Graph.scala
@@ -1,9 +1,29 @@
 package fr.paris.lutece.plugins.scala.bean.graph
 
-class Graph {
+class Graph(name:String="Graph", bidirectional:Boolean=true) {
   var nodes: Map[String,Node] = Map()
 
   def shortestWay(node1: String, node2: String): List[String] = {
+    null
+  }
+  
+  def addNode(nameNode:String){
+    
+  }
+  
+  def addLink(name1:String,name2:String,distance:Int){
+    
+  }
+  
+  def removeLink(name1:String, name2:String){
+    
+  }
+  
+  def distance(node1:String, node2:String):Int={
+    -1
+  }
+  
+  def seeNeighbors(name:String):Map[String,Int]={
     null
   }
 }
