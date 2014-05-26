0d318be401e8c84a713d847246ef87ee71b9fe07
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-05-26 20:23:03 +0000

    Begin to test future keyword

--- lucette2/src/main/scala/battle/csn/lucette2/game/logic/NegaMax.scala
+++ lucette2/src/main/scala/battle/csn/lucette2/game/logic/NegaMax.scala
@@ -1,52 +1,61 @@
 package battle.csn.lucette2.game.logic
 
-
 import org.springframework.stereotype.Service
 import scala.collection.JavaConverters._
 import battle.csn.lucette2.game.board.Board
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
+import scala.util.{ Failure, Success }
+import scala.util.Try
+import scala.util.control.Breaks._
 
 class NegaMax extends Logic {
 
   private var cpt = 0
-  
-    def getCpt():Int={cpt}
+
+  def getCpt(): Int = { cpt }
+
+  def solveR(player: Int, plateau: Board[Int], alpha: Integer, beta: Integer, heuristique: (Int, Board[Int]) => Int, findMax: Boolean, deep: Integer): Int = {
+    val s = future {
+      solveR(player, plateau, alpha, beta, heuristique, findMax, deep)
+    }
+    var res = 0
+    s onComplete {
+      case Success(value) => res = value
+      case _ =>
+    }
+    res
+  }
 
   /**
-     * this algorithm can only use with black & white players games
-     */
-    def solve(player:Int, plateau : Board[Int], alpha:Integer, beta:Integer, heuristique:(Int , Board[Int]) => Int, findMax:Boolean, deep: Integer):Int={
-        cpt+=1
-        var value = 0;
-        var moves = plateau.moveAvailables(player)
+   * this algorithm can only use with black & white players games
+   */
+  def solve(player: Int, plateau: Board[Int], alpha: Integer, beta: Integer, heuristique: (Int, Board[Int]) => Int, findMax: Boolean, deep: Integer): Int = {
+    cpt += 1
+    var value = 0;
+    var moves = plateau.moveAvailables(player)
 
-        //si le plateau est une feuille
-        if ( moves.size == 0 || deep == 0 ) 
-        {
-            value = heuristique(player, plateau)
-        }
-        else
-        {
-            var bestScore = if (findMax) Integer.MIN_VALUE else Integer.MAX_VALUE
-            //Pour toutes les coups jouables du plateau
-            for ( move <- moves )
-            {
-                var deepCopy = plateau.deepCopy( );
-                deepCopy.play( player,move );
-                var score = solve(player * -1, deepCopy, -beta, -alpha, heuristique, !findMax, deep - 1 );
-                if ( ( findMax && score > bestScore ) || ( !findMax && score < bestScore ) )
-                {
-                    bestScore = score;
-                    if ( ( findMax && bestScore > alpha ) || ( !findMax && bestScore < alpha ) )
-                    {
-                        if ( ( findMax && bestScore >= beta ) || ( findMax && bestScore <= beta ) )
-                        {
-                            value = bestScore;
-                            //break;
-                        }
-                    }
-                }
+    //si le plateau est une feuille
+    if (moves.size == 0 || deep == 0) {
+      value = heuristique(player, plateau)
+    } else {
+      var bestScore = if (findMax) Integer.MIN_VALUE else Integer.MAX_VALUE
+      //Pour toutes les coups jouables du plateau
+      for (move <- moves) {
+        var deepCopy = plateau.deepCopy()
+        deepCopy.play(player, move)
+        var score = solve(player * -1, deepCopy, -beta, -alpha, heuristique, !findMax, deep - 1)
+        if ((findMax && score > bestScore) || (!findMax && score < bestScore)) {
+          bestScore = score
+          if ((findMax && bestScore > alpha) || (!findMax && bestScore < alpha)) {
+            if ((findMax && bestScore >= beta) || (findMax && bestScore <= beta)) {
+              value = bestScore
+              break
             }
+          }
         }
-        value
+      }
     }
+    value
+  }
 }
