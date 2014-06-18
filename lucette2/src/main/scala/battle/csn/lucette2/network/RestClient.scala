e30f23c9e6449944c8de797973eb0c286c601b40
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-06-18 06:42:56 +0000

    Translate the client part, error with Apache Option

--- lucette2/src/main/scala/battle/csn/lucette2/network/RestClient.scala
+++ lucette2/src/main/scala/battle/csn/lucette2/network/RestClient.scala
@@ -2,84 +2,82 @@ package battle.csn.lucette2.network
 
 class RestClient extends WsClient {
 
-    
-    var BASE_URL = "http://localhost:8080/test"
-    //    private static String BASE_URL = "http://ec2-54-200-12-98.us-west-2.compute.amazonaws.com/csnbattlearena/webservices/test";
+  var BASE_URL = "http://localhost:8080/test"
+  //    private static String BASE_URL = "http://ec2-54-200-12-98.us-west-2.compute.amazonaws.com/csnbattlearena/webservices/test";
 
-    var URI_SEPARATOR = "/";
-    var PING_URI = "/ping"
-    var PING_ERROR_500 = "/pingError500"
-    var PING_ERROR_403 = "/pingError403"
-    var GET_ID_EQUIPE_URI = "/player/getIdEquipe/"
-    var STATUS_URI = "/game/status/"
-    var BOARD_URI = "/game/board/"
-    var LAST_MOVE_URI = "/game/getlastmove/"
-    var PLAY_URI = "/game/play/"
+  var URI_SEPARATOR = "/";
+  var PING_URI = "/ping"
+  var PING_ERROR_500 = "/pingError500"
+  var PING_ERROR_403 = "/pingError403"
+  var GET_ID_EQUIPE_URI = "/player/getIdEquipe/"
+  var STATUS_URI = "/game/status/"
+  var BOARD_URI = "/game/board/"
+  var LAST_MOVE_URI = "/game/getlastmove/"
+  var PLAY_URI = "/game/play/"
 
-    var CREATE_GAME_URI = "/practice/new/"
-    var GET_ID_GAME_URI = "/versus/next/"
-      
-      var client:HttpLayerAPI=_
-      
-      {
-      client = new HttpLayerAPI( BASE_URL )
+  var CREATE_GAME_URI = "/practice/new/"
+  var GET_ID_GAME_URI = "/versus/next/"
+
+  var client: HttpLayerAPI = _
+
+  {
+    client = new HttpLayerAPI(BASE_URL)
+  }
+
+  @Override
+  def ping() =
+    {
+      client.callString(PING_URI);
+    }
+
+  @Override
+  def getIdEquipe(nomEquipe: String, motDePasse: String) =
+    {
+      var getIdEquipe = GET_ID_EQUIPE_URI + nomEquipe + URI_SEPARATOR + motDePasse
+      client.callString(getIdEquipe);
+    }
+
+  @Override
+  def getGameStatus(idPartie: String, idEquipe: String) =
+    {
+      var getStatusUri = STATUS_URI + idPartie + URI_SEPARATOR + idEquipe;
+      client.callString(getStatusUri);
+    }
+
+  @Override
+  def getBoard(idPartie: String) =
+    {
+      var getBoard = BOARD_URI + idPartie;
+      client.callString(getBoard);
+    }
+
+  @Override
+  def getNewPracticeGame(idEquipe: String, level: Int) =
+    {
+      var getNewPractiveGame = CREATE_GAME_URI + level + URI_SEPARATOR + idEquipe;
+      client.callString(getNewPractiveGame);
+    }
+
+  @Override
+  def getGameId(idEquipe: String) =
+    {
+      client.callString(GET_ID_GAME_URI);
+    }
+
+  @Override
+  def getLastMove(idPartie: String) =
+    {
+      var getLastMove = LAST_MOVE_URI + idPartie;
+      client.callString(getLastMove);
+    }
+
+  @Override
+  def play(idPartie: String, idEquipe: String, coords: Seq[Int]) =
+    {
+      var playMove = PLAY_URI + idPartie + URI_SEPARATOR + idEquipe;
+      for (coord <- coords) {
+        playMove += URI_SEPARATOR + String.valueOf(coord);
       }
-    
-    @Override
-    def ping( )=
-    {
-        client.callString(PING_URI );
-    }
-
-    @Override
-    def getIdEquipe( nomEquipe:String, motDePasse :String)=
-    {
-        var getIdEquipe = GET_ID_EQUIPE_URI + nomEquipe + URI_SEPARATOR + motDePasse
-        client.callString( getIdEquipe );
-    }
-
-    @Override
-    def getGameStatus(idPartie:String, idEquipe :String)=
-    {
-        var getStatusUri = STATUS_URI + idPartie + URI_SEPARATOR + idEquipe;
-        client.callString( getStatusUri );
-    }
-
-    @Override
-    def getBoard(  idPartie :String)=
-    {
-        var getBoard = BOARD_URI + idPartie;
-        client.callString( getBoard );
-    }
-
-    @Override
-    def getNewPracticeGame(  idEquipe:String, level :Int)=
-    {
-        var getNewPractiveGame = CREATE_GAME_URI + level + URI_SEPARATOR + idEquipe;
-        client.callString( getNewPractiveGame );
-    }
-
-    @Override
-    def getGameId(  idEquipe :String)=
-    {
-        client.callString( GET_ID_GAME_URI );
-    }
-
-    @Override
-    def getLastMove( idPartie:String )=
-    {
-        var getLastMove = LAST_MOVE_URI + idPartie;
-        client.callString( getLastMove );
-    }
-
-    @Override
-    def play( idPartie:String, idEquipe:String, coords:Seq[Int] )=
-    {
-        var playMove = PLAY_URI + idPartie + URI_SEPARATOR + idEquipe;
-        for ( coord <- coords )
-        {
-            playMove += URI_SEPARATOR + String.valueOf( coord );
-        }
-        client.callString( playMove );
+      client.callString(playMove);
     }
 }
