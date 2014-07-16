224c6452f1011cbf08656f2ced5d59f22a0741d4
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-07-16 14:47:13 +0000

    Add enumeration examples

---
+++ MyScalaProject/src/main/java/fr/chaline/scala/myproject/enumeration/Monster.scala
@@ -0,0 +1,5 @@
+package fr.chaline.scala.myproject.enumeration
+
+class Monster {
+  var typeMonster = TypeMonster.Normal
+}
---
+++ MyScalaProject/src/main/java/fr/chaline/scala/myproject/enumeration/TypeMonster.scala
@@ -0,0 +1,6 @@
+package fr.chaline.scala.myproject.enumeration
+
+object TypeMonster extends Enumeration {
+  type TypeMonster = Value
+  val Fire, Water, Plant, Normal = Value
+}
