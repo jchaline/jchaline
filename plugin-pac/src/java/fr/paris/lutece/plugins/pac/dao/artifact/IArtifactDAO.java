27675ddd1310f0bf7a122bc9f69b3d41db890ab7
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-08-01 07:01:46 +0000

    Remove artifact from plugin-pac

--- plugin-pac/src/java/fr/paris/lutece/plugins/pac/dao/artifact/IArtifactDAO.java
+++
@@ -1,9 +0,0 @@
-package fr.paris.lutece.plugins.pac.dao.artifact;
-
-import fr.paris.lutece.plugins.genericjpa.dao.IPluginDAO;
-import fr.paris.lutece.plugins.pac.bean.artifact.Artifact;
-
-public interface IArtifactDAO extends IPluginDAO<Integer, Artifact>
-{
-
-}
