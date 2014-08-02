dcdc9f24339e849aafc00e84cd5d196a85cb2789
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-08-02 21:48:29 +0000

    Can use hibernate with scala artifact bean

--- plugin-scala/src/main/scala/fr/paris/lutece/plugins/scala/xpage/RepositoryApp.scala
+++ plugin-scala/src/main/scala/fr/paris/lutece/plugins/scala/xpage/RepositoryApp.scala
@@ -1,22 +1,23 @@
 package fr.paris.lutece.plugins.scala.xpage
 
+import org.apache.log4j.Logger
+
+import fr.paris.lutece.plugins.genericjpa.service.IPluginService
+import fr.paris.lutece.plugins.scala.bean.graph.Graph
 import fr.paris.lutece.plugins.scala.service.artifact.ArtifactService
 import fr.paris.lutece.plugins.scala.service.artifact.IArtifactService
 import fr.paris.lutece.portal.service.plugin.Plugin
+import fr.paris.lutece.portal.service.spring.SpringContextService
 import fr.paris.lutece.portal.service.template.AppTemplateService
 import fr.paris.lutece.portal.web.xpages.AbstractXPageApplication
 import fr.paris.lutece.portal.web.xpages.XPage
 import javax.servlet.http.HttpServletRequest
-import fr.paris.lutece.portal.service.util.AppLogService
-import fr.paris.lutece.portal.service.spring.SpringContextService
-import fr.paris.lutece.plugins.scala.bean.graph.Graph
-import org.apache.log4j.Logger
 
 class RepositoryApp extends AbstractXPageApplication {
 
   var TEMPLATE_REPOSITORY = "skin/plugins/scala/repository/repository.html";
 
-  var artifactService = SpringContextService.getContext().getBean(classOf[IArtifactService])
+  var artifactService:IArtifactService = SpringContextService.getBean("scala.artifactservice")
 
   var LOGGER: Logger = Logger.getLogger(classOf[RepositoryApp])
 
@@ -56,6 +57,9 @@ class RepositoryApp extends AbstractXPageApplication {
 
     var artifact = artifactService.findByStrPrimaryKey("")
     println(artifact)
+    
+    var list = artifactService.findAll(null)
+    print(list)
 
     var template = AppTemplateService.getTemplate(TEMPLATE_REPOSITORY, request.getLocale(), model);
 
