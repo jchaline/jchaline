9170f48a1105586f681d8391414d2d01828b8def
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-08-02 21:48:29 +0000

    Can use hibernate with scala artifact bean

--- trunk/plugin-scala/src/main/scala/fr/paris/lutece/plugins/scala/service/artifact/ArtifactService.scala
+++ trunk/plugin-scala/src/main/scala/fr/paris/lutece/plugins/scala/service/artifact/ArtifactService.scala
@@ -1,15 +1,15 @@
 package fr.paris.lutece.plugins.scala.service.artifact
 
-import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 
 import fr.paris.lutece.plugins.genericjpa.dao.IPluginDAO
+import fr.paris.lutece.plugins.genericjpa.service.AbstractService
 import fr.paris.lutece.plugins.scala.bean.artifact.Artifact
 import fr.paris.lutece.plugins.scala.dao.artifact.ArtifactDAO
 import javax.inject.Inject
 
 @Service("scala.artifactservice")
-class ArtifactService extends IArtifactService {
+class ArtifactService extends AbstractService[Int,Artifact] with IArtifactService{
 
   @Inject
   var artifactDAO=null:ArtifactDAO
--- trunk/plugin-scala/src/main/scala/fr/paris/lutece/plugins/scala/service/artifact/IArtifactService.scala
+++ trunk/plugin-scala/src/main/scala/fr/paris/lutece/plugins/scala/service/artifact/IArtifactService.scala
@@ -4,5 +4,5 @@ import fr.paris.lutece.plugins.genericjpa.service.IPluginService
 import fr.paris.lutece.plugins.genericjpa.service.AbstractService
 import fr.paris.lutece.plugins.scala.bean.artifact.Artifact
 
-trait IArtifactService extends AbstractService[Int,Artifact]{
+trait IArtifactService extends IPluginService[Int,Artifact] {
 }
