6007b1590eed4c2391118088f1b5b5c31ab11b8b
chaline.jeremy@gmail.com <chaline.jeremy@gmail.com@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-09-05 06:02:10 +0000

    Update first domain and use auto scafolding

--- pacmaster/grails-app/domain/pacmaster/Customer.groovy
+++ pacmaster/grails-app/domain/pacmaster/Customer.groovy
@@ -2,9 +2,16 @@ package pacmaster
 
 class Customer {
 
-	String firstName, lastName
-	Date firstDay
+	String firstName, lastName, email
+	Date arrival, nextPac, lastPac
 	
     static constraints = {
+		firstName()
+		lastName()
+		email()
+		arrival()
+		lastPac()
+		nextPac()
+		
     }
 }
