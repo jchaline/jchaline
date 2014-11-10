324efb17a416c97769a0e15f6b53ef9c3d0074d2
chaline.jeremy <chaline.jeremy@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-11-10 17:05:53 +0000

    put db conf into db.properties file

--- trunk/grails/cra/grails-app/conf/Config.groovy
+++ trunk/grails/cra/grails-app/conf/Config.groovy
@@ -4,22 +4,17 @@ grails.config.defaults.locations = [
         fr.cg92.core.config.CGDefaultConfig
 ]
 
-
-// locations to search for config files that get merged into the main config;
-// config files can be ConfigSlurper scripts, Java properties files, or classes
-// in the classpath in ConfigSlurper format
-
-// grails.config.locations = [ "classpath:${appName}-config.properties",
-//                             "classpath:${appName}-config.groovy",
-//                             "file:${userHome}/.grails/${appName}-config.properties",
-//                             "file:${userHome}/.grails/${appName}-config.groovy"]
-
-// if (System.properties["${appName}.config.location"]) {
-//    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
-// }
+envName = "${appName}-${grails.util.Environment.current.name}"
+configPath = "${envName}-config.properties"
+dbPath = "${envName}-db.properties"
+println("Begin")
+println("config : "+configPath)
+println("db : "+dbPath)
+println("end")
+grails.config.locations = ["classpath:${configPath}","classpath:db/${dbPath}"]
 
 def projectRules = [
-        '/collaborateur/**':                              [SecRole.ADMINISTRATEUR],
+        '/collaborateur/**':                       [SecRole.ADMINISTRATEUR],
         '/projet/**':                              ['permitAll'],
 ]
 
