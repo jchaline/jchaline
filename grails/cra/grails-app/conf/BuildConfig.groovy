8f4335317e885b22654d918901ddbd8f27e34570
chaline.jeremy <chaline.jeremy@e2e358a1-c3a5-5c30-0f45-651ad65eccec>
2014-11-10 17:05:53 +0000

    put db conf into db.properties file

--- grails/cra/grails-app/conf/BuildConfig.groovy
+++ grails/cra/grails-app/conf/BuildConfig.groovy
@@ -6,19 +6,12 @@ grails.project.work.dir = "target/work"
 grails.project.target.level = 1.8
 grails.project.source.level = 1.8
 grails.project.war.file = "target/${appName}.war"
-grails.assets.minifyJs = false
 
+forkConfig = false
 grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-
-    // configure settings for the test-app JVM, uses the daemon by default
-    test: false,
-    // configure settings for the run-app JVM
-    run: false,
-    // configure settings for the run-war JVM
+    test: forkConfig,
+    run: forkConfig,
     war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the Console UI JVM
     console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ]
 
@@ -41,22 +34,17 @@ grails.project.dependency.resolution = {
         mavenLocal()
         grailsCentral()
         mavenCentral()
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         // runtime 'mysql:mysql-connector-java:5.1.29'
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
-        runtime 'mysql:mysql-connector-java:5.1.27'
+        runtime 'mysql:mysql-connector-java:5.1.29'
         test "org.grails:grails-datastore-test-support:1.0-grails-2.4"
     }
 
     plugins {
-        // plugins for the build system only
         build ":tomcat:7.0.55"
 
         compile "fr.cg92.socle:cg-core:1.0.0-SNAPSHOT"
